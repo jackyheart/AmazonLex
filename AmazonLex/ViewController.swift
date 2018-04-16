57f06e11a5bcd965c4292d03248f6f4da3d2acb2
Jacky Tjoa <jacky@pixerf.com>
2018-04-16 16:03:19 +0800

    first commit

--- AmazonLex/ViewController.swift
+++ AmazonLex/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import AWSCore
+import AWSLex
+import AWSMobileClient
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var awsLexVoiceButton: AWSLexVoiceButton!
+    @IBOutlet weak var responseLbl: UILabel!
+    
     override func viewDidLoad() {
+        
+        // Set the bot configuration details
+        // You can use the configuration constants defined in AWSConfiguration.swift file
+        let botName = "OrderFlowersOneMOBILEHUB"
+        let botRegion: AWSRegionType = .USEast1
+        let botAlias = "$LATEST"
+        
+        // set up the configuration for AWS Voice Button
+        let configuration = AWSServiceConfiguration(region: botRegion, credentialsProvider: AWSMobileClient.sharedInstance().getCredentialsProvider())
+        let botConfig = AWSLexInteractionKitConfig.defaultInteractionKitConfig(withBotName: botName, botAlias: botAlias)
+        
+        // register the interaction kit client for the voice button using the AWSLexVoiceButtonKey constant defined in SDK
+        AWSLexInteractionKit.register(with: configuration!, interactionKitConfiguration: botConfig, forKey: AWSLexVoiceButtonKey)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        (self.awsLexVoiceButton as AWSLexVoiceButton).delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: AWSLexVoiceButtonDelegate {
+    
+    func voiceButton(_ button: AWSLexVoiceButton, on response: AWSLexVoiceButtonResponse) {
+        DispatchQueue.main.async {
+            self.responseLbl.text = "\(response.outputText ?? "")"
+        }
+    }
+    
+    func voiceButton(_ button: AWSLexVoiceButton, onError error: Error) {
+        DispatchQueue.main.async {
+            self.responseLbl.text = "\(error.localizedDescription)"
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'AmazonLex' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'AWSMobileClient', '~> 2.6.13'
+  pod 'AWSLex', '~> 2.6.13'
+
+  # Pods for AmazonLex
+
+end
